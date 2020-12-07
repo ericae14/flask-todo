4bebc9cbd01903db3d7d1a211b9ef5d28596cd6a
Erica Edwards <ericae14@hotmail.com>
2020-12-06 16:23:41 -0800

    Fixing spacing

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 
 @app.route('/all')
 def all_tasks():
- return render_template('all.jinja2', tasks=Task.select())
+    return render_template('all.jinja2', tasks=Task.select())
 
 if __name__ == "__main__":
- port = int(os.environ.get("PORT", 5000))
- app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
